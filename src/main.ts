3eaf8209c0e302d9ffda9bd391f4b59f51f1f5d4
이정현 <junghyun@junghyun-MacBookPro.local>
2025-01-02 22:37:18 +0900

    docs: whitelist, forbidNonWhitelisted 설명 추가

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 유효성 검사용 파이프
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true, // "property hacked should not exist"
+    whitelist: true, // class-validator의 유효성 검사 데코레이터를 사용하지 않은 모든 속성 객체를 제거되어 저장
+    forbidNonWhitelisted: true, // "property hacked should not exist" : HttpException 을 던짐
     transform: true, // 타입을 받아서 넘겨줄 때(default는 string) 자동으로 실제 타입으로 변환해줌
   }));
   await app.listen(process.env.PORT ?? 3000);
