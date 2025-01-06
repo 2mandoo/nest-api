8488ad3aa5cfdc720e124cea1031f98c30d3f0f8
junghyun <dlwjdgus7441@gmail.com>
2025-01-06 19:16:58 +0900

    feat: 테스팅 환경과 실제 환경의 설정 동기화

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    // 테스팅 환경에서 만들어지는 테스트를 위한 애플리케이션과 브라우저에서 테스트 할 수 있는 진짜 애플리케이션은 다름
+    // => 테스팅 환경도 실제 구동 환경의 설정을 그대로 적용시켜줘야 함
+    app.useGlobalPipes(new ValidationPipe({
+        whitelist: true, // class-validator의 유효성 검사 데코레이터를 사용하지 않은 모든 속성 객체를 제거되어 저장
+        forbidNonWhitelisted: true, // "property hacked should not exist" : HttpException 을 던짐
+        transform: true, // 타입을 받아서 넘겨줄 때(default는 string) 자동으로 실제 타입으로 변환해줌
+      }));
+      
     await app.init();
   });
 
@@ -41,6 +50,7 @@ describe('AppController (e2e)', () => {
       .expect(201);
     })
 
+    // 404 나오는지 테스트
     it("DELETE", () => {
       return request(app.getHttpServer())
       .delete('/movies')
@@ -48,5 +58,14 @@ describe('AppController (e2e)', () => {
     });
 
   });
+
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get("/movies/1").expect(200);
+    })
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get("/movies/999").expect(404);
+    })
+  })
 
 });
